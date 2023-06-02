b2045615ba38ef01bad57b1253a83dc16dd8ed68
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-06-02 13:01:20 -0300

    Create LerDados.java

---
+++ Java/Intros/LerDados.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a;
+        char b;
+        int c;
+        double d;
+        
+        a = scan.nextLine();
+        System.out.println(""+ a);
+        
+        b = scan.next().charAt(0);
+        System.out.println(""+ b);
+        
+        c = scan.nextInt();
+        System.out.println(""+ c);
+        
+        d = scan.nextDouble();
+        System.out.println(""+ d);
+    }
+}
