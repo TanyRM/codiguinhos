f748d31e7f1b078fe6728a34d081baeabe36da50
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-06-14 12:05:40 -0300

    Create Contato.java

---
+++ Java/Projetos/agenda/Contato.java
@@ -0,0 +1,35 @@
+package agenda.src;
+
+public class Contato {
+  public String nome;
+  public String telefone;
+  public String email;
+  
+  public Contato(String nome, String telefone, String email){
+    this.nome = nome;
+    this.telefone = telefone;
+    this.email = email;
+  }
+  public void exibirContato(Contato contato){
+    System.out.println(contato.nome);
+    System.out.println(contato.telefone);
+  	System.out.println(contato.email);
+  }
+  public void compararNome(String nome, Contato contato){
+    if (nome.equals(contato.nome)){
+      exibirContato(contato);
+    }
+  }
+  /*public void exibirAgenda(){
+    System.out.println(contato1.nome);
+    System.out.println(contato1.telefone);
+    System.out.println(contato1.email);
+    System.out.println(contato2.nome);
+    System.out.println(contato2.telefone);
+    System.out.println(contato2.email);
+    System.out.println(contato3.nome);
+    System.out.println(contato3.telefone);
+    System.out.println(contato3.email);
+  }*/
+  
+}
