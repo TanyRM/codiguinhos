3e0b8b990c88139c3b845253620dde9f213d3ff1
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-06-14 12:09:12 -0300

    Create Horario.java

---
+++ Java/Projetos/curso/Horario.java
@@ -0,0 +1,19 @@
+package curso.src;
+
+public class Horario {
+  private String dias;
+  private int horas;
+  
+  public void setDias(String dias){
+	this.dias = dias;
+  }
+  public String getDias(){
+    return this.dias;
+  }
+  public void setHoras(int horas){
+	this.horas = horas;
+  }
+  public int getHoras(){
+    return this.horas;
+  }
+}
