f61743252b9c90915b0668113080badbe2caa48e
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-06-14 12:10:11 -0300

    Create Aluno.java

---
+++ Java/Projetos/curso/Aluno.java
@@ -0,0 +1,27 @@
+package curso.src;
+
+public class Aluno {
+  private String nomea;
+  private String matricula;
+  private double[] notas;
+  public double media = 0;
+  
+  public void setNomea(String nomea){
+	this.nomea = nomea;
+  }
+  public String getNomea(){
+    return nomea;
+  }
+  public void setMatricula(String matricula){
+	this.matricula = matricula;
+  }
+  public String getMatricula(){
+    return this.matricula;
+  }
+  public void setNotas(double[] notas){
+	this.notas = notas;
+  }
+  public double[] getNotas(){
+    return this.notas;
+  }
+}
