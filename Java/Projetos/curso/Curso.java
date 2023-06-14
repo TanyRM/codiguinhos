5732bfe1b366f7c5c40f9ab36ecd4f503823e653
Taniele <127350142+TanyRM@users.noreply.github.com>
2023-06-14 12:09:44 -0300

    Create Curso.java

---
+++ Java/Projetos/curso/Curso.java
@@ -0,0 +1,43 @@
+package curso.src;
+import curso.src.Aluno;
+import curso.src.Professor;
+import curso.src.Horario;
+
+public class Curso {
+  private String nome;
+  private Horario horario;
+  private Professor professor;
+  private Aluno[] alunos;
+  private double media = 0;
+  
+  public void setNome(String nome){
+	this.nome = nome;
+  }
+  public String getNome(){
+    return this.nome;
+  }
+  public void setHorario(Horario horario){
+    this.horario = horario;
+  }
+  public Horario getHorario(){
+    return horario;
+  }
+  public void setProfessor(Professor professor){
+    this.professor = professor;
+  }
+  public Professor getProfessor(){
+    return professor;
+  }
+  public void setAlunos(Aluno[] alunos){
+    this.alunos = alunos;
+  }
+  public Aluno[] getAlunos(){
+    return alunos;
+  }
+  public void setMedia(double media){
+    this.media = media;
+  }
+  public double getMedia(){
+    return media;
+  }
+}
