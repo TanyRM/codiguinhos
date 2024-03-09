047ef051b905cd19df9b77ce968eede0f3dad77c
TanyRM <tanielemadu@outlook.com>
2024-03-09 18:56:39 -0300

    collections

--- kotlin/introducao/collections/Maps.kt
+++ kotlin/introducao/collections/Maps.kt
@@ -1,3 +1,35 @@
 package introducao.collections
 
 // Maps: Conjuntos de pares de valores-chave em que as chaves são exclusivas e mapeadas para apenas um valor
+fun main(){
+    /* Map armazena os itens como pares de valores, o valor é acessado referenciando a chave
+    - são uteis para procurar valores sem usar indice numerado
+    - permite valores duplicados
+     */
+
+    val readOnlyMenu = mapOf("maçã" to 5, "uva" to 20, "banana" to 5) // Map com valores imutaveis
+    println(readOnlyMenu)
+
+    val sucoMenu: MutableMap<String, Int> = mutableMapOf("uva" to 5, "maçã" to 10, "laranja" to 5) // Map mutavel com declaração explicita de tipos <chave, valor>
+    println(sucoMenu)
+
+    val sucosBloqueado: Map<String, Int> = sucoMenu
+
+    println("O valor da uva é ${readOnlyMenu["uva"]}") // Para retornar o valor se usa a chave como indice
+
+    println("Esse mapa tem ${readOnlyMenu.count()} opções")
+
+    sucoMenu.put("caju", 5) // Pode-se adicionar valores usando .put(chave, valor)
+    sucoMenu["acerola"] = 10 // Ou, passando a chave e inicializando o valor em seguida
+    println(sucoMenu)
+
+    sucoMenu.remove("maçã") // Para remover se usa o .remove(chave)
+    println(sucoMenu)
+
+    println(readOnlyMenu.containsKey("uva")) // Para verificar se uma chave está no mapa
+    println("uva" in readOnlyMenu) // ou println("uva" in readOnlyMenu.keys)
+    println(10 in readOnlyMenu.values) // Verifica se um valor está no mapa
+
+    println(readOnlyMenu.keys) // Retorna uma lista das chaves do mapa
+    println(readOnlyMenu.values) // Retorna uma lista com os valores do mapa
+}
--- kotlin/introducao/collections/Sets.kt
+++ kotlin/introducao/collections/Sets.kt
@@ -4,5 +4,22 @@ package introducao.collections
 
 fun main(){
     // Sets não são ordenadas e permitem apenas elementos unicos
+    val readyOnlySet = setOf(1, 2, 3, 4, 5) // Set de valor imutavel
+    println(readyOnlySet) // O item duplicado aparece apenas uma vez
 
+    val frutas: MutableSet<String> = mutableSetOf("maçã", "uva", "laranja", "uva") // Set mutavel com declaração de tipo explicita
+    println(frutas)
+
+    val numBloqueados: Set<String> = frutas
+
+    // Em sets não se pode acessar itens pelo indice
+    println("Esse set tem ${readyOnlySet.count()} frutas")
+
+    println("banana" in frutas)
+
+    frutas.add("banana")
+    frutas.remove("uva")
+    println(frutas)
+
+    println(frutas.size)
 }
